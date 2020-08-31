1de919a7dcf4c8231c427c898579ca9c9bda8d31
gwue <gwue89@gmail.com>
2020-08-31 13:47:08 +0000

    Comments

--- primes/src/main.rs
+++ primes/src/main.rs
@@ -5,6 +5,7 @@ fn main() {
     const N: u64 = 500_000;
     let now = SystemTime::now();
     let primes = calc_primes(N);
+
     match now.elapsed() {
         Ok(elapsed) => {
             println!("Time: {}ms", elapsed.as_millis());
@@ -12,7 +13,7 @@ fn main() {
         Err(_) => {}
     }
 
-    println!("{}", primes.iter().last().map_or(0, |&v| v));
+    println!("The {}th prime number is {}", N, primes.iter().last().map_or(0, |&v| v));
 }
 
 // Returns the first n primes
@@ -21,9 +22,7 @@ fn calc_primes(n: u64) -> Vec<u64> {
 
     let mut upper_bound = 2;
     loop {
-        //println!("Calc primes until upper bound {}", upper_bound);
         let mut primes = sieve_up_to(upper_bound, &mut pre_sieved);
-        //println!("Primes for upper bound {} : {:?}", upper_bound, primes);
         upper_bound *= 2;
         if primes.len() >= n as usize {
             primes.truncate(n as usize);
@@ -34,74 +33,58 @@ fn calc_primes(n: u64) -> Vec<u64> {
 
 // Returns all primes up to a given boundary n using the sieve of Eratosthenes
 // The underlying sieve is returned and can be reused for future iterations using a greater boundary
+// sieve[i] == 0 -> i is not a prime
+// sieve[i] == i -> i is prime
 fn sieve_up_to(n: u64, sieve: &mut Vec<u64>) -> Vec<u64> {
+    // Init minimal sieve
     if sieve.len() < 3 {
         sieve.clear();
         sieve.append(&mut vec![0, 0, 2]);
     }
 
+    let previous_length = sieve.len() as u64;
     let last_prime: u64 = match sieve.iter().rev().find(|&&x| x != 0) {
         Some(p) => *p,
         None => 2,
     };
 
-    //println!("Last prime: {}", last_prime);
-
+    // Given sieve is expanded such that sieve[i] == i --> expanded area still needs to be tested
     let mut val_to_add = sieve.len() as u64 - 1;
     sieve.resize_with(n as usize, || {
         val_to_add += 1;
         val_to_add
     });
 
-    //println!("Mark non-primes from already known primes in resized area");
-    // Mark non-primes from already known primes in resized area
+    // Mark non-primes from already known primes in expanded area
     for prime_index in 2..last_prime {
-        //println!("Prime Index: {}",prime_index);
-        let mut factor = max(prime_index, last_prime / prime_index); // XXX
         if sieve[prime_index as usize] != 0 {
+            // Start with a factor, such that prime*factor starts closer to the beginning of the expanded area
+            // Example: * previous sieve = [0,0,2,3,0,5]
+            //          * current sieve = [0,0,2,3,0,5,6,7,8,9,10,11]
+            //          * prime_index = 2
+            //          * factor starts at 3 so we skip marking 4 as non-prime (we already know it isn't)
+            //let mut factor = max(prime_index, last_prime / prime_index);
+            let mut factor = max(prime_index, previous_length / prime_index);
             while prime_index * factor < sieve.len() as u64 {
-                /*
-                println!(
-                    "Index {} * Factor {} = {}",
-                    prime_index,
-                    factor,
-                    prime_index * factor
-                );
-                */
                 sieve[(prime_index * factor) as usize] = 0;
                 factor += 1;
             }
         }
     }
 
-    // println!("Mark remaining non-primes");
+    // Continue to mark non-primes from those that have been newly found
     for prime_index in last_prime..sieve.len() as u64 {
-        //   println!("Prime Index: {}",prime_index);
-        let mut factor = max(2, prime_index);
         if sieve[prime_index as usize] != 0 {
+            // Start with the factor as the square of the current prime number (default algorithm for Eratosthenes)
+            let mut factor = max(2, prime_index);
             while prime_index * factor < sieve.len() as u64 {
-                /*
-                println!(
-                    "Index {} * Factor {} = {}",
-                    prime_index,
-                    factor,
-                    prime_index * factor
-                );*/
                 sieve[(prime_index * factor) as usize] = 0;
-
                 factor += 1;
             }
         }
     }
 
     let primes: Vec<u64> = sieve.iter().filter(|&&x| x != 0).map(|x| *x).collect();
-    /*
-       let mut primes = Vec::new();
-        for number in sieve {
-            if *number != 0 {
-                primes.push(*number);
-            }
-        }
-    */
+
     primes
 }
