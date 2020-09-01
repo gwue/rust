0b0fdc2fa25776d970840f19953b26d09651032e
gwue <gwue89@gmail.com>
2020-09-01 11:38:15 +0000

    sum-of-multiples with iterator trait

--- sum-of-multiples/src/lib.rs
+++ sum-of-multiples/src/lib.rs
@@ -1,7 +1,39 @@
+use std::collections::HashSet;
+
 pub fn sum_of_multiples(limit: u32, factors: &[u32]) -> u32 {
-    unimplemented!(
-        "Sum the multiples of all of {:?} which are less than {}",
-        factors,
-        limit
-    )
+    let mut distinct_multiples = HashSet::new();
+
+    for f in factors {
+        for m in multiples_of(*f).take_while(|&x| x < limit) {
+            distinct_multiples.insert(m);
+        }
+    }
+
+    distinct_multiples.iter().sum()
+}
+
+struct Multiples {
+    of: u32,
+    multiplier: u32,
+}
+
+impl Iterator for Multiples {
+    type Item = u32;
+
+    fn next(&mut self) -> Option<u32> {
+        let current_multiplier = self.multiplier;
+        self.multiplier += 1;
+        println!("Of: {}, Current mul: {}", self.of, current_multiplier);
+
+        match self.of {
+            0 => None,
+            _ => Some(self.of * current_multiplier),
+        }
+    }
+}
+fn multiples_of(n: u32) -> Multiples {
+    Multiples {
+        of: n,
+        multiplier: 1,
+    }
 }
