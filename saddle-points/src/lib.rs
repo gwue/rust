83644eb3d27dacb74914c1de0d02974364209ae1
gwue <gwue89@gmail.com>
2020-09-08 08:10:46 +0000

    Saddle points

--- saddle-points/src/lib.rs
+++ saddle-points/src/lib.rs
@@ -1,6 +1,33 @@
+use std::cmp::min;
+
 pub fn find_saddle_points(input: &[Vec<u64>]) -> Vec<(usize, usize)> {
-    unimplemented!(
-        "find the saddle points of the following matrix: {:?}",
-        input
-    )
+    let mut output = Vec::new();
+
+    for row in 0..input.len() {
+        let row_max = row_max(input, row);
+        println!("Max in row {} = {}", row, row_max);
+
+        for col in 0..input[row].len() {
+            let col_min = col_min(input, col);
+            println!("Min in col {} = {}", col, col_min);
+            let val = input[row][col];
+            if val >= row_max && val <= col_min {
+                output.push((row, col));
+            }
+        }
+    }
+
+    output
+}
+
+fn row_max(input: &[Vec<u64>], row: usize) -> u64 {
+    *input[row].iter().max().unwrap_or(&u64::MIN)
+}
+
+fn col_min(input: &[Vec<u64>], col: usize) -> u64 {
+    let mut c_min = u64::MAX;
+    for row in input {
+        c_min = min(row[col], c_min);
+    }
+    c_min
 }
