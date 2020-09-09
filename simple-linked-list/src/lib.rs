e1316b8c3821e3fac7ccbadfaedc2228142ba91f
gwue <gwue89@gmail.com>
2020-09-09 08:00:05 +0200

    Linked list

--- simple-linked-list/src/lib.rs
+++ simple-linked-list/src/lib.rs
@@ -1,14 +1,34 @@
 use std::iter::FromIterator;
 
 pub struct SimpleLinkedList<T> {
-    // Delete this field
-    // dummy is needed to avoid unused parameter error during compilation
-    dummy: ::std::marker::PhantomData<T>,
+    head: Box<Option<Node<T>>>,
+    size: usize,
+}
+
+struct Node<T> {
+    data: T,
+    next: Box<Option<Node<T>>>,
+}
+
+impl<T> Node<T> {
+    fn new(val:T) -> Self {
+        Node {
+            data: val,
+            next: Box::new(None)
+        }
+    }
+
+    fn set_next(&mut self, val:T) {
+        *self.next = Some(Node::new(val));
+    }
 }
 
 impl<T> SimpleLinkedList<T> {
     pub fn new() -> Self {
-        unimplemented!()
+        SimpleLinkedList {
+            head: Box::new(None),
+            size: 0
+        }
     }
 
     pub fn len(&self) -> usize {
@@ -16,7 +36,26 @@ impl<T> SimpleLinkedList<T> {
     }
 
     pub fn push(&mut self, _element: T) {
-        unimplemented!()
+        match &*self.head {
+            None => {
+                *self.head = Some(Node::new(_element));
+                self.size += 1;
+            },
+            Some(n) => {
+                let mut running_node = n;
+                // Find the tail
+                loop {
+                    match &*running_node.next {
+                        Some(next_node) => {
+                            running_node = next_node;
+                        },
+                        None => {
+                            running_node.set_next(_element);
+                        }
+                    }
+                }
+            },
+        }
     }
 
     pub fn pop(&mut self) -> Option<T> {
